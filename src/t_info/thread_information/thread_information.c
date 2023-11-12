c77b7c8398d228122b0826aa23e6f856d4e6ed1e
IndianMax03 <vermamanu@mail.ru>
2023-11-12 20:38:40 +0300

    Add: debug messages

--- src/t_info/thread_information/thread_information.c
+++ src/t_info/thread_information/thread_information.c
@@ -11,16 +11,19 @@ SYSCALL_DEFINE2(gimme_thread_info, int, pid, void*, data)
 
     task = pid_task(find_vpid(pid), PIDTYPE_PID);
     if (task == NULL) {
+        printk("ERR: ESRCH");
         return -ESRCH; //  Процесс не найден
     }
 
     info = task_thread_info(task);
     if (info == NULL) {
+        printk("ERR: EINVAL");
         return -EINVAL; //  Аргумент передан неверно
     }
 
     struct custom_thread_info *c_t_i = kmalloc(sizeof(struct custom_thread_info), GFP_KERNEL);
     if (c_t_i == NULL) {
+        printk("ERR: ENOMEM");
         return -ENOMEM; // Недостаточно памяти
     }
 
@@ -38,6 +41,7 @@ SYSCALL_DEFINE2(gimme_thread_info, int, pid, void*, data)
         c_t_i->task_state = info->task->__state;
         c_t_i->task_prio = info->task->prio;
     } else {
+        printk("WAR: info->task is null");
         c_t_i->task_state = 0;
         c_t_i->task_prio = -1;
     }
@@ -56,9 +60,11 @@ SYSCALL_DEFINE2(gimme_thread_info, int, pid, void*, data)
     
     if (copy_to_user(data, c_t_i, sizeof(struct custom_thread_info)) != 0) {
         kfree(c_t_i);
+        printk("ERR: EFAULT");
         return -EFAULT; //  Неверный адрес
     }
 
     kfree(c_t_i);
+    printk("INF: OK");
     return 0;
 }
--- src/vm_area/vm_area_information/vm_area_information.c
+++ src/vm_area/vm_area_information/vm_area_information.c
@@ -10,16 +10,19 @@ SYSCALL_DEFINE3(gimme_vm_area_info, int, pid, unsigned long, addr, void*, data)
 
     task = pid_task(find_vpid(pid), PIDTYPE_PID);
     if (task == NULL) {
+        printk("ERR: ESRCH");
         return -ESRCH; //  Процесс не найден
     }
 
     struct vm_area_struct *vm_info = find_vma(task->active_mm, addr);
     if (vm_info == NULL) {
+        printk("ERR: EINVAL (while finding)");
         return -EINVAL; //  Неверный аргумент (адрес)
     }
 
     struct custom_vm_area_info *c_va_i = kmalloc(sizeof(struct custom_vm_area_info), GFP_KERNEL);
     if (c_va_i == NULL) {
+        printk("ERR: EINVAL (while kmalloc)");
         return -EINVAL; //  Неверный аргумент (адрес)
     }
 
@@ -33,6 +36,7 @@ SYSCALL_DEFINE3(gimme_vm_area_info, int, pid, unsigned long, addr, void*, data)
         c_va_i->vm_region_top = vm_info->vm_region->vm_top;
         c_va_i->vm_region_pgoff = vm_info->vm_region->vm_top;
     } else {
+        printk("WARN: vm_info->vm_region is null");
         c_va_i->vm_region_start = 0;
         c_va_i->vm_region_end = 0;
         c_va_i->vm_region_top = 0;
@@ -41,9 +45,11 @@ SYSCALL_DEFINE3(gimme_vm_area_info, int, pid, unsigned long, addr, void*, data)
     
     if (copy_to_user(data, c_va_i, sizeof(struct custom_vm_area_info)) != 0) {
         kfree(c_va_i);
+        printk("ERR: EFAULT");
         return -EFAULT; //  Неверный адрес
     }
 
     kfree(c_va_i);
+    printk("INF: OK");
     return 0;
 }
