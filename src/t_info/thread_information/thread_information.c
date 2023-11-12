2d7d6caa04bcbfd9e860245a592e9cde03629449
IndianMax03 <vermamanu@mail.ru>
2023-11-12 21:51:40 +0300

    Update: tscall data

--- src/t_info/thread_information/thread_information.c
+++ src/t_info/thread_information/thread_information.c
@@ -27,35 +27,35 @@ SYSCALL_DEFINE2(gimme_thread_info, int, pid, void*, data)
         return -ENOMEM; // Недостаточно памяти
     }
 
-    с_t_i->palcode_ksp = info->pcb_struct->ksp;
-    с_t_i->palcode_usp = info->pcb_struct->usp;
-    с_t_i->palcode_ptbr = info->pcb_struct->ptbr;
-    с_t_i->palcode_pcc = info->pcb_struct->pcc;
-    с_t_i->palcode_asn = info->pcb_struct->asn;
-    с_t_i->palcode_unique = info->pcb_struct->unique;
-    с_t_i->palcode_flags = info->pcb_struct->flags;
-    с_t_i->palcode_res1 = info->pcb_struct->res1;
-    с_t_i->palcode_res2 = info->pcb_struct->res2;
+    // c_t_i->palcode_ksp = info->pcb->ksp;
+    // c_t_i->palcode_usp = info->pcb->usp;
+    // c_t_i->palcode_ptbr = info->pcb->ptbr;
+    // c_t_i->palcode_pcc = info->pcb->pcc;
+    // c_t_i->palcode_asn = info->pcb->asn;
+    // c_t_i->palcode_unique = info->pcb->unique;
+    // c_t_i->palcode_flags = info->pcb->flags;
+    // c_t_i->palcode_res1 = info->pcb->res1;
+    // c_t_i->palcode_res2 = info->pcb->res2;
 
-    if (info->task != NULL) {
-        c_t_i->task_state = info->task->__state;
-        c_t_i->task_prio = info->task->prio;
-    } else {
-        printk("WAR: info->task is null");
-        c_t_i->task_state = 0;
-        c_t_i->task_prio = -1;
-    }
+    // if (info->task != NULL) {
+    //     c_t_i->task_state = info->task->__state;
+    //     c_t_i->task_prio = info->task->prio;
+    // } else {
+    //     printk("WAR: info->task is null");
+    //     c_t_i->task_state = 0;
+    //     c_t_i->task_prio = -1;
+    // }
     
     c_t_i->flags = info->flags;
-    c_t_i->ieee_state = info->ieee_state;
+    // c_t_i->ieee_state = info->ieee_state;
     c_t_i->cpu = info->cpu;
-    c_t_i->preempt_count = info->preempt_count;
+    // c_t_i->preempt_count = info->preempt_count;
     c_t_i->status = info->status;
-    c_t_i->bpt_nsaved = info->bpt_nsaved;
-    c_t_i->bpt_addr[0] = info->bpt_addr[0];
-    c_t_i->bpt_addr[1] = info->bpt_addr[1];
-    c_t_i->bpt_insn[0] = info->bpt_insn[0];
-    c_t_i->bpt_insn[1] = info->bpt_insn[1];
+    // c_t_i->bpt_nsaved = info->bpt_nsaved;
+    // c_t_i->bpt_addr[0] = info->bpt_addr[0];
+    // c_t_i->bpt_addr[1] = info->bpt_addr[1];
+    // c_t_i->bpt_insn[0] = info->bpt_insn[0];
+    // c_t_i->bpt_insn[1] = info->bpt_insn[1];
 
     
     if (copy_to_user(data, c_t_i, sizeof(struct custom_thread_info)) != 0) {
