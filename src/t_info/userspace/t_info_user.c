26c91466e5f85076284f67df1a77e8cf638356d7
IndianMax03 <vermamanu@mail.ru>
2023-11-13 05:34:42 +0300

    Update: reporting side

--- src/t_info/userspace/t_info_user.c
+++ src/t_info/userspace/t_info_user.c
@@ -5,33 +5,45 @@
 #include <unistd.h>
 #include "custom_thread_info.h"
 
-#define     ERROR   -1
-#define     OK      0
+#define     ERROR               -1
+#define     OK                  0
+#define     GIMME_THREAD_INFO   548
 
 int main(int argc, char* arcgv[])
 {
 
-  if (argc != 2) {
-    printf("Invalid usage\n");
-    printf("try: ./t_info_user <pid>\n");
-    return ERROR;
-  }
+    if (argc != 2) {
+        printf("Invalid usage\n");
+        printf("try: ./prog <pid>\n");
+        return ERROR;
+    }
 
-  int pid = atoi(arcgv[1]);
+    int pid = atoi(arcgv[1]);
 
-  struct custom_thread_info* data = malloc(sizeof(struct custom_thread_info));
+    struct custom_thread_info* data = malloc(sizeof(struct custom_thread_info));
 
-  long int retval = syscall(548, pid, data);
+    if (data == NULL) {
+        printf("Cannot allocate memory.\n");
+        return ERROR;
+    }
 
-  if (retval != 0) {
-    printf("try another pid!\n");
-  }
+    long int retval = syscall(GIMME_THREAD_INFO, pid, data);
 
-  printf("scall reted %ld\n", retval);
-  printf("%lu\n", data->flags);
-  printf("%lu\n", data->syscall_work);
-  printf("%u\n",data->status);
-  printf("%u\n",data->cpu);
+    if (retval != 0) {
+        free(data);
+        printf("Error while syscalling, broo\n");
+        printf("exec: sudo dmesg\n");
+        return ERROR;
+    }
+  
+    printf("thread_struct:\n");
+    printf("--- flags = %lu\n", data->flags);
+    printf("--- syscall_work = %lu\n", data->syscall_work);
+    printf("--- status = %u\n",data->status);
+    printf("--- cpu = %u\n",data->cpu);
 
-  return OK;
+    free(data);
+
+    return OK;
+
 }
