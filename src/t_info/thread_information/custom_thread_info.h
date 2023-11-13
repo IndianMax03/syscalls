57570ad561ff44884739d62cfe358f066ae1ecf7
IndianMax03 <vermamanu@mail.ru>
2023-11-13 03:59:21 +0300

    Fix: thread info structure

--- src/t_info/thread_information/custom_thread_info.h
+++ src/t_info/thread_information/custom_thread_info.h
@@ -3,25 +3,10 @@
 
 struct custom_thread_info
 {
-    // unsigned long   palcode_ksp;
-	// unsigned long   palcode_usp;
-	// unsigned long   palcode_ptbr;
-	// unsigned int    palcode_pcc;
-	// unsigned int    palcode_asn;
-	// unsigned long   palcode_unique;
-	// unsigned long   palcode_flags;
-	// unsigned long   palcode_res1, palcode_res2;
-    // unsigned int    task_state;
-    // int             task_prio;
-    unsigned int    flags;
-    // unsigned int    ieee_state;
-    unsigned        cpu;
-    // int             preempt_count;
+    unsigned long   flags;
+    unsigned long   syscall_work;
     unsigned int    status;
-    // int             bpt_nsaved;
-    // unsigned long   bpt_addr[2];
-    // unsigned int    bpt_insn[2];
+    unsigned int    cpu;
 };
-
 
 #endif
